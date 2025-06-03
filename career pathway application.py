0a68a3ea89fb2398d4cc20c69613dc99b85c9d3b
sanchit-jain08 <149262985+sanchit-jain08@users.noreply.github.com>
2025-06-03 16:50:26 +0530

    Update career pathway application.py

--- career pathway application.py
+++ career pathway application.py
@@ -43,7 +43,8 @@ st.markdown("""
         .header-container {
             background-color: #f0f2f6;
             padding: 30px;
-            border-radius: 10px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
             margin-bottom: 0px;
             text-align: center;
             box-shadow: 0 4px 8px rgba(0,0,0,0.05);
